97448b007cc178151fbe4ee82f8cb0d59fe31fb8
Sean Brown <inphomercial@gmail.com>
2014-09-29 13:30:57 -0600

    got directories working, added images, got bookmark totals working, etc

--- bookmarkerizer.js
+++ bookmarkerizer.js
@@ -20,34 +20,46 @@ function removeBookmark(id) {
 function dumpNodesToArray(bookmark_nodes) {
 
 	var bookmarks = [];
-	var directories = [];
-
-	console.log("BOOKMARK NODES");
-	console.log(bookmark_nodes);
-
 	bookmarks = getChildNodes(bookmarks, bookmark_nodes);
 
-	console.log(bookmarks);
-
 	return bookmarks;
+}
+
+function dumpDirectoriesToArray(bookmark_nodes) {
+
+	var directories = [];
+	directories = getDirectoryNodes(directories, bookmark_nodes);
+
+	return directories;
+}
+
+function getDirectoryNodes(master, array) {
+
+	for( var i=0; i<array.length; i++) {
+		if(array[i].children && array[i].children.length > 0) {
+			master.push(array[i]);
+			getDirectoryNodes(master, array[i].children);
+		}
+		else
+		{
+		}
+	}
+
+	return master;
 }
 
 function getChildNodes(master, array) {
 
 	for( var i=0; i<array.length; i++) {
 		if(array[i].children && array[i].children.length > 0) {
-			console.log("found dir");
 			getChildNodes(master, array[i].children);
 		}
 		else
 		{
-			console.log("found bookmark");
 			master.push(array[i]);
 		}
 	}
 
-	console.log("RETURN MASTER");
-	console.log(master);
 	return master;
 }
 
@@ -56,26 +68,65 @@ function process_bookmark(bookmarks) {
 	var bookmark = bookmarks[0];
 	var bookmark_bar = bookmark.children[0];
 
-	// Bookmark Bar
-	console.log(bookmark_bar);
-
 	// Start button disabled
 	$('#cleanup').attr('disabled', 'disabled');
 
-
 	var bookmarks = dumpNodesToArray(bookmark_bar.children);
-	console.log("FINAL OUTPUT ARRAY");
-	console.log(bookmarks);
+	var directories = dumpDirectoriesToArray(bookmark_bar.children);
 
 	// Sorts by Date by reference
 	orderByDate(bookmarks);
+	
+	console.log("FINAL BOOKMARKS ARRAY");
+	console.log(bookmarks);
 
+	console.log("FINAL DIRECTORIES ARRAY");
+	console.log(directories);
+
+	// List of directories
+	for(var i=0; i<directories.length; i++) {
+		var tr = $('<tr class="directory_id warning">');
+
+		// Done td
+		tr.append('<td class="id">' + directories[i].id + '</td>');
+
+		// Image td
+		var img_td = $('<td>');
+		var img = $('<img>');
+		img.attr('src', "folder.png");
+		img_td.append(img);
+		tr.append(img_td);
+
+		// Date added
+		var dt = new Date(directories[i].dateAdded);
+		var month = dt.getMonth()+1;
+		tr.append('<td>' + month + '/' + dt.getDate() + '/' + dt.getFullYear() + '</td>');
+		
+		// Url td
+		var td = $('<td>');
+		var txt = $('<p>');
+    		txt.text(directories[i].title + " - contains : " + directories[i].children.length);
+		td.append(txt);
+		tr.append(td);
+
+		// Add tr to table
+		$('.list').append(tr);
+	}
+
+	// List of bookmarks
 	for(var i=0; i<bookmarks.length; i++) {
 
 		var tr = $('<tr class="bookmark_id">');
 
 		// Done td
 		tr.append('<td class="id">' + bookmarks[i].id + '</td>');
+
+		// Image td
+		var img_td = $('<td>');
+		var img = $('<img>');
+		img.attr('src', "bookmark.png");
+		img_td.append(img);
+		tr.append(img_td);
 
 		// Date added
 		var dt = new Date(bookmarks[i].dateAdded);
@@ -97,6 +148,9 @@ function process_bookmark(bookmarks) {
 		$('.id').hide();
 	}
 
+	// Get Total
+	$('.total_value').text(bookmarks.length);
+
 	$('.bookmark_id').on("click", function(e) {
 		// Toggle danger class (marked for deletion)
 		$(this).toggleClass('danger');
@@ -113,6 +167,7 @@ function process_bookmark(bookmarks) {
 	});
 
 	$('#cleanup').on('click', function() {
+
 		var all = $('.danger');
 
 		var ids_array = [];
@@ -126,6 +181,14 @@ function process_bookmark(bookmarks) {
 		// Hide the deleted slowly for dramatic effect!
 		all.hide('slow');
 		$(this).attr('disabled', 'disabled');
+
+		// Get Total
+		var old_value = $('.total_value').text();
+		var new_value = old_value - all.length;
+		$('.total_value').text(new_value);
+
+		// Remove old deleted to keep numbers clean if additional deleting takes place
+		all.remove();
 	});
 }
 
